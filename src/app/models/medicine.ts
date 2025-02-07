c3474bddf28cb5e74d5fea7796b0ac6a4a1d4f20
chiaraapastore <chiarapastoree001@gmail.com>
2025-02-07 01:52:11 +0100

    Initial Commit

---
+++ src/app/models/medicine.ts
@@ -0,0 +1,10 @@
+export interface Medicine {
+  id: number;
+  nome: string;
+  scadenza: string;
+  categoria: string;
+  puntoRiordino: number;
+  descrizione: string;
+  quantita: number;
+  availableQuantity: number;
+}
