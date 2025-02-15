244d8171eeb1c73fcd5ee753ebddfd6afb0e1a57
chiaraapastore <chiarapastoree001@gmail.com>
2025-02-15 20:34:29 +0100

    Initial Commit

---
+++ src/app/event-dialog/event-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-event-dialog',
+  templateUrl: './event-dialog.component.html',
+  styleUrls: ['./event-dialog.component.css']
+})
+export class EventDialogComponent {
+  eventTitle: string = '';
+
+  constructor(
+    public dialogRef: MatDialogRef<EventDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    this.dialogRef.close(this.eventTitle);
+  }
+}
